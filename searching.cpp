e563f9f068b46cd1f5cc58af6f1a2c35feb1b542
Rachit Shrivastava <rshriva@ncsu.edu>
2017-11-07 12:27:04 -0500

    Binary Search

--- searching.cpp
+++ searching.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int linearSearch(int arr[], int x) {
-	int n = sizeof(arr)/sizeof(arr[0]);
+int linearSearch(vector<int> arr, int x) {
+	int n = arr.size();
 	for(int i=0; i<n; i++) {
 		if(x == arr[i]) {
 			return i;
@@ -12,29 +14,55 @@ int linearSearch(int arr[], int x) {
 	return -1;
 }
 
+int binarySearch(vector<int> arr, int x) {
+	//sort first
+	sort(arr.begin(), arr.end());
+	int n = arr.size();
+	int l = 0;
+	int r = n-1;
+
+	while(l <= r) {
+		int mid = l + (r-l)/2;
+
+		if(arr[mid] == x) {
+			return mid;
+		}
+
+		if(arr[mid] > x) {
+			r = mid - 1;
+		} else {
+			l = mid + 1;
+		}
+	}
+	
+	return -1;
+}
+
 int main() {
-	int i, n, pos;
+	int i, n;
 
 	cout<<"Enter size of array: ";
 	cin>>n;
 
-	int arr[n];
+	vector<int> arr;
 	i=0;
 
-	while(i<n) {
-		cin>>arr[i];
-		i++;
+	while(n > 0) {
+		cin>>i;
+		arr.push_back(i);
+		n--;
 	}
 	
 	cout<<"Input number to be searched: ";
 	cin>>i;
 
-	pos = linearSearch(arr, i);
+	//pos = linearSearch(arr, i);
+	n = binarySearch(arr, i);
 
-	if(pos == -1) {
+	if(n == -1) {
 		cout<<i<<" not found in array.";
 	} else {
-		cout<<i<<" found at "<<pos;
+		cout<<i<<" found at "<<n;
 	}
 	
 	return 0;
