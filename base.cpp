98846e5c8a054680c1df733d2aa3303f128f1e32
Rachit Shrivastava <rshriva@ncsu.edu>
2017-11-07 17:22:57 -0500

    Base - modified

--- base.cpp
+++ base.cpp
@@ -7,5 +7,22 @@
 using namespace std;
 
 int main() {
+	int i, n;
+	vector<int> arr;
+
+	cout<<"Enter n Value: ";
+	cin>>n;
+
+	while(n > 0) {
+		cin>>i;
+		arr.push_back(i);
+		n--;
+	}
+
+	cout<<"Result: ";
+	for(int i=0; i<arr.size(); i++) {
+		cout<<arr[i]<<" ";
+	}
+
     return 0;
 }
